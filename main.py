0ac4eea49325e65ee28ead8cfff87dbe7d7496c7
HardikJain202001051 <94235599+HardikJain202001051@users.noreply.github.com>
2021-12-25 13:53:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ TOKEN = '1402226931:AAGKobkwOobTQPkKcby6M7u9pz3C6mg-zEI'
 API_Key = '0fVMGzefYUNsNgDWZoLUFEx4F'
 API_Key_Secret = 'LrWwApOzu9Z1IPgbHGXjmAPoAySVhnjaX5uDjVez5diVsYMVmL'
 allowed_users = [1462381549, 1518770169, 1144079321, 1243579615, 1505554149, 1498556745]
+auth = tweepy.OAuthHandler(API_Key, API_Key_Secret)
+api = tweepy.API(auth)
 
-
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(options=chrome_options,executable_path=ChromeDriverManager().install())
+      
 def welcome(update, context):
     update.message.reply_text("Hey!Send the link of tweet to get the video")
     #r = 'https://api.telegram.org/bot'+TOKEN+'/sendMessage?chat_id='+'@DevelopmentNewsIndia'+'&text=Hello World!'
@@ -19,17 +25,12 @@ def message_action(update, context):
     if update.message.chat_id in allowed_users:
         link = update.message.text
         ids = link.split('/')[-1].split('?')[0]
-        auth = tweepy.OAuthHandler(API_Key, API_Key_Secret)
-        api = tweepy.API(auth)
+        
         status = api.get_status(id=ids, tweet_mode="extended")
         cap = status.full_text
         site = "https://twittervideodownloader.com"
         #s = Service("D:\SeleniumDriverChrome\chromedriver.exe")
         #driver = webdriver.Chrome(service=s)
-        chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(options=chrome_options,executable_path=ChromeDriverManager().install())
         driver.get(site)
         field = driver.find_element(By.NAME, 'tweet')
         field.send_keys(link)
