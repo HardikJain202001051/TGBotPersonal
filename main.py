05161838004451891f76fbff9f99a46fd55977af
HardikJain202001051 <94235599+HardikJain202001051@users.noreply.github.com>
2021-12-26 17:21:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from selenium.webdriver.common.by import By
 from telegram.ext import Updater, MessageHandler, CommandHandler, Filters
+import requests
 from selenium import webdriver
 # from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import tweepy
 from selenium import webdriver
 
-TOKEN = '5027998873:AAHaa9EOETEwvUAVFsX0almIMw-wr9jJVm4'
+TOKEN = '1402226931:AAGKobkwOobTQPkKcby6M7u9pz3C6mg-zEI'
+API_Key = '0fVMGzefYUNsNgDWZoLUFEx4F'
+API_Key_Secret = 'LrWwApOzu9Z1IPgbHGXjmAPoAySVhnjaX5uDjVez5diVsYMVmL'
+allowed_users = [1462381549, 1518770169, 1144079321, 1243579615, 1505554149, 1498556745]
+auth = tweepy.OAuthHandler(API_Key, API_Key_Secret)
+api = tweepy.API(auth)
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
@@ -13,11 +20,15 @@ driver = webdriver.Chrome(options=chrome_options)
       
 def welcome(update, context):
     update.message.reply_text("Hey!Send the link of tweet to get the video")
+    #r = 'https://api.telegram.org/bot'+TOKEN+'/sendMessage?chat_id='+'@DevelopmentNewsIndia'+'&text=Hello World!'
 
-allowed_users = [1518770169,1243219058]
 def message_action(update, context):
     if update.message.chat_id in allowed_users:
-        link = update.message.text        
+        link = update.message.text
+        ids = link.split('/')[-1].split('?')[0]
+        
+        status = api.get_status(id=ids, tweet_mode="extended")
+        cap = status.full_text
         site = "https://twittervideodownloader.com"
         #s = Service("D:\SeleniumDriverChrome\chromedriver.exe")
         #driver = webdriver.Chrome(service=s)
@@ -27,9 +38,12 @@ def message_action(update, context):
         submit = driver.find_element(By.CLASS_NAME, 'input-group-button')
         submit.click()
         d = driver.find_element(By.LINK_TEXT,"Download Video")
-        link = d.get_attribute('href')
-        update.message.reply_video(video=link)
-            
+        link2 = d.get_attribute('href')
+        r = 'https://api.telegram.org/bot' + TOKEN + '/sendVideo?chat_id=@DevelopmentNewsIndia&caption='+cap+'&video='+link2
+        requests.post(r)
+        #update.message.reply_video(video=link2,caption=cap)
+    else:
+        update.message.reply_text("Sorry, you are not an authorized user")
 updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 start = CommandHandler('start', welcome)
